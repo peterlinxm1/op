a103459fc9e6e7d195a42516ad617ad783b87d08
ophub <68696949+ophub@users.noreply.github.com>
2020-07-30 13:56:36 +0800

    Update diy-part1.sh

--- router/phicomm_n1/diy-part1.sh
+++ router/phicomm_n1/diy-part1.sh
@@ -14,10 +14,6 @@ git clone https://github.com/tuanqing/install-program \
     package/install-program
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-bootstrap-mod \
     package/luci-theme-bootstrap-mod
-svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-argon-dark-mod \
-    package/luci-theme-argon-dark-mod
-svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-argon-light-mod \
-    package/luci-theme-argon-light-mod
 
 
 
