fb46aab3f1d7d28874b601eaa3e072168072373c
ophub <68696949+ophub@users.noreply.github.com>
2020-07-31 16:32:48 +0800

    Update diy-part1.sh

--- router/phicomm_n1/diy-part1.sh
+++ router/phicomm_n1/diy-part1.sh
@@ -8,7 +8,7 @@
 #=============================================================
 
 # Mydiy program/luci-app/theme（use to /.config luci-app&theme）install to lede/package
-git clone https://github.com/tuanqing/install-program package/install-program
+svn co https://github.com/ophub/op/trunk/router/phicomm_n1/install-program package/install-program
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-bootstrap-mod \
     package/luci-theme-bootstrap-mod
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-argon-dark-mod \
