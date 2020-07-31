5281063caee20ba84a3d1f363f793b537befc07d
ophub <68696949+ophub@users.noreply.github.com>
2020-07-31 15:50:57 +0800

    Update diy-part1.sh

--- router/phicomm_n1/diy-part1.sh
+++ router/phicomm_n1/diy-part1.sh
@@ -8,8 +8,13 @@
 #=============================================================
 
 # Mydiy program/luci-app/theme（use to /.config luci-app&theme）install to lede/package
-git clone https://github.com/ophub/op/trunk/router/phicomm_n1/install-program package/install-program
-svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-bootstrap-mod package/luci-theme-bootstrap-mod
+git clone https://github.com/tuanqing/install-program package/install-program
+svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-bootstrap-mod \
+    package/luci-theme-bootstrap-mod
+svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-argon-dark-mod \
+    package/luci-theme-argon-dark-mod
+svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-argon-light-mod \
+    package/luci-theme-argon-light-mod
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
