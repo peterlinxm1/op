9078e0ec16e6d929a30a7a321683e666c32afec6
ophub <68696949+ophub@users.noreply.github.com>
2020-07-31 16:02:26 +0800

    Update diy-part2.sh

--- router/phicomm_n1/diy-part2.sh
+++ router/phicomm_n1/diy-part2.sh
@@ -30,7 +30,7 @@
 #git apply lede-mod/luci/*.patch --directory=feeds/luci
 #git apply lede-mod/bootstrap/*.patch --directory=package/luci-theme-bootstrap-mod
 
-patches="../../router/phicomm_n1/lede-mod"
+patches="lede-mod"
 echo "patching feeds luci"
 git apply $patches/luci/*.patch --directory=feeds/luci
 echo "patching luci-theme-bootstrap-mod"
