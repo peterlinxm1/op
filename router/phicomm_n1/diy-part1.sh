6983f5f11848e53fcd6d488f17c547bc978ecfaa
ophub <68696949+ophub@users.noreply.github.com>
2020-07-31 15:06:59 +0800

    Update diy-part1.sh

--- router/phicomm_n1/diy-part1.sh
+++ router/phicomm_n1/diy-part1.sh
@@ -8,22 +8,12 @@
 #=============================================================
 
 
-# Mydiy program/luci-app/theme（use to /.config luci-app&theme）
-# install to lede/package
-svn co https://github.com/ophub/op/trunk/router/phicomm_n1/install-program package/install-program
+# Mydiy program/luci-app/theme（use to /.config luci-app&theme）install to lede/package
+git clone https://github.com/ophub/op/trunk/router/phicomm_n1/install-program package/install-program
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-bootstrap-mod package/luci-theme-bootstrap-mod
-
-
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-packages.git;dev-master' feeds.conf.default
-#sed -i '$a src-git luci https://github.com/Lienol/openwrt-luci.git;dev-18.06' feeds.conf.default
-
 
 #other
 rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
