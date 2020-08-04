efb84ead7389db329af18f256a796fe56b1793b6
ophub <68696949+ophub@users.noreply.github.com>
2020-08-04 10:29:25 +0800

    Update diy-part2.sh

--- router/x64/diy-part2.sh
+++ router/x64/diy-part2.sh
@@ -17,12 +17,12 @@
 
 
 # Mydiy-luci-app-and-theme（use to /.config luci-app&theme）
-# cd package
-# mkdir openwrt-packages
-# cd openwrt-packages
 # ==========luci-app-url==========
-# git clone https://github.com/kenzok8/openwrt-packages.git
-# git clone https://github.com/kenzok8/small.git
-# git clone https://github.com/fw876/helloworld.git
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+# git clone https://github.com/kenzok8/small.git package/small
+# git clone https://github.com/fw876/helloworld.git package/helloworld
 # ==========luci-theme-url==========
-# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+# svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-theme-edge package/luci-theme-edge
+
+
