5152955be7f40560d3827c6373ea4853ece26227
ophub <68696949+ophub@users.noreply.github.com>
2020-08-04 10:28:45 +0800

    Update diy-part2.sh

--- router/phicomm_n1/diy-part2.sh
+++ router/phicomm_n1/diy-part2.sh
@@ -15,16 +15,16 @@
 # Modify default root's password（FROM 'password'[$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.] CHANGE TO 'your password'）
 # sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
-# install to openwrt/package/openwrt-packages(mkdirFolder)
-#cd package
-#mkdir openwrt-packages
-#cd openwrt-packages
+# Mydiy-luci-app-and-theme（use to /.config luci-app&theme）
 # ==========luci-app-url==========
-# git clone https://github.com/kenzok8/openwrt-packages.git
-# git clone https://github.com/kenzok8/small.git
-# git clone https://github.com/fw876/helloworld.git
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+# git clone https://github.com/kenzok8/small.git package/small
+# git clone https://github.com/fw876/helloworld.git package/helloworld
 # ==========luci-theme-url==========
-#git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+# svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-theme-edge package/luci-theme-edge
+
+
 
 svn co https://github.com/ophub/op/trunk/router/phicomm_n1/lede-mod
 git apply lede-mod/luci/*.patch --directory=feeds/luci
