1c820c168a74f76fe135b4f0c889c0ea3148fd70
ophub <68696949+ophub@users.noreply.github.com>
2020-08-04 10:54:53 +0800

    Update diy-part1.sh

--- router/phicomm_n1/diy-part1.sh
+++ router/phicomm_n1/diy-part1.sh
@@ -7,6 +7,14 @@
 # Author: P3TERX
 #=============================================================
 
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+
 # Mydiy program/luci-app/theme（use to /.config luci-app&theme）install to lede/package
 svn co https://github.com/ophub/op/trunk/router/phicomm_n1/install-program package/install-program
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-bootstrap-mod \
@@ -16,12 +24,6 @@ svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-argon-d
 svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-argon-light-mod \
     package/luci-theme-argon-light-mod
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
-
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #other
 rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
