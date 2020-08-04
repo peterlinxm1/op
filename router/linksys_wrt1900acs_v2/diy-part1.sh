79848ceca2f2b5db59d151b3f99acc0acbbd3481
ophub <68696949+ophub@users.noreply.github.com>
2020-08-04 10:50:38 +0800

    Update diy-part1.sh

--- router/linksys_wrt1900acs_v2/diy-part1.sh
+++ router/linksys_wrt1900acs_v2/diy-part1.sh
@@ -11,6 +11,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i 's/\"#src-git\"/\"src-git\"/g' ./feeds.conf.default
 
+
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
