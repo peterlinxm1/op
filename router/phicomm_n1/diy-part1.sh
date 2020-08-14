93937d255f48bbf25e831da1b4aca563450987d2
ophub <68696949+ophub@users.noreply.github.com>
2020-08-14 15:44:06 +0800

    Update diy-part1.sh

--- router/phicomm_n1/diy-part1.sh
+++ router/phicomm_n1/diy-part1.sh
@@ -14,14 +14,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-
-# Add program/luci-app/theme（use to /.config luci-app&theme）install to openwrt/package
-svn co https://github.com/ophub/op/trunk/router/phicomm_n1/install-program package/install-program
-svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-bootstrap-mod package/luci-theme-bootstrap-mod
-#svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-argon-dark-mod package/luci-theme-argon-dark-mod
-#svn co https://github.com/Lienol/openwrt-package/trunk/lienol/luci-theme-argon-light-mod package/luci-theme-argon-light-mod
-
-
 #other
 #rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
