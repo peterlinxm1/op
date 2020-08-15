a7552c126706d8d1bd0e79ba2578fa1a9832cdc6
ophub <68696949+ophub@users.noreply.github.com>
2020-08-15 13:07:16 +0800

    Update diy-part1.sh

--- router/phicomm_n1/diy-part1.sh
+++ router/phicomm_n1/diy-part1.sh
@@ -9,11 +9,12 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
+# sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
+# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-#other
-#rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+# other
+# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
