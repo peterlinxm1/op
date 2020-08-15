0fb429eb728c5b3ed3cdbd4560d6238cbbbb7f63
ophub <68696949+ophub@users.noreply.github.com>
2020-08-15 13:08:09 +0800

    Update diy-part1.sh

--- router/x64/diy-part1.sh
+++ router/x64/diy-part1.sh
@@ -10,9 +10,10 @@
 
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-#sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
+# sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
+# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
