a3d0cd0799914099239be760c75fccf94b7437dd
ophub <68696949+ophub@users.noreply.github.com>
2020-08-27 14:50:33 +0800

    Create diy-part2.sh

---
+++ router/linksys_wrt3200acm/diy-part2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#=================================================
+# https://github.com/ophub/op
+# Description: Automatically Build OpenWrt for Linksys WRT1900ACS
+# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+#=================================================
+
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
+# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+# Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
+# sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+# Modify default root's password（FROM 'password'[$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.] CHANGE TO 'your password'）
+# sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+
+# Mydiy-luci-app-and-theme（use to /.config luci-app&theme）
+# ==========luci-app-url==========
+# git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+# git clone https://github.com/kenzok8/small.git package/small
+# git clone https://github.com/fw876/helloworld.git package/helloworld
+# ==========luci-theme-url==========
+# git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+# svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-theme-edge package/luci-theme-edge
+
