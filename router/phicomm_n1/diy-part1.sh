03aaf263504854973d707830025ea77303721eb5
ophub <68696949+ophub@users.noreply.github.com>
2020-09-07 11:23:46 +0800

    Update diy-part1.sh

--- router/phicomm_n1/diy-part1.sh
+++ router/phicomm_n1/diy-part1.sh
@@ -3,6 +3,9 @@
 # https://github.com/ophub/op
 # Description: Automatically Build OpenWrt for PHICOMM N1
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Copyright (C) 2020 https://github.com/tuanqing/mknop
+# Copyright (C) 2020 https://github.com/P3TERX/Actions-OpenWrt
+# Copyright (C) 2020 https://github.com/ophub/op
 #=================================================
 
 # Uncomment a feed source
