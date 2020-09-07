ac92a2cfd4402326d2d384bb9926b64f9d4ce24e
ophub <68696949+ophub@users.noreply.github.com>
2020-09-07 11:30:01 +0800

    Update diy-part2.sh

--- router/linksys_wrt3200acm/diy-part2.sh
+++ router/linksys_wrt3200acm/diy-part2.sh
@@ -3,6 +3,8 @@
 # https://github.com/ophub/op
 # Description: Automatically Build OpenWrt for Linksys WRT3200ACM
 # Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Copyright (C) 2020 https://github.com/P3TERX/Actions-OpenWrt
+# Copyright (C) 2020 https://github.com/ophub/op
 #=================================================
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
