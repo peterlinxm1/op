4a4956994d6f5bb0c3ea0e6a7fd088e69276da10
ophub <68696949+ophub@users.noreply.github.com>
2020-09-07 11:28:37 +0800

    Update diy-part1.sh

--- router/linksys_wrt32x/diy-part1.sh
+++ router/linksys_wrt32x/diy-part1.sh
@@ -3,6 +3,8 @@
 # https://github.com/ophub/op
 # Description: Automatically Build OpenWrt for Linksys WRT32X
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Copyright (C) 2020 https://github.com/P3TERX/Actions-OpenWrt
+# Copyright (C) 2020 https://github.com/ophub/op
 #=================================================
 
 # Uncomment a feed source
